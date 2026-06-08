a02ae5344ae8d49364f764d6f371b1a66ae3ee4a
Namox-0 <danielombardero@gmail.com>
2026-06-08 09:33:47 +0200

    Delete Scripts directory

--- Scripts/al.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-deluser jefe
-deluser admin1
-deluser admin2
-delgroup administradores
-
-rm -r /home/jefe/
--- Scripts/userlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Nombre usuario: " user_name
-if grep -q $user_name /etc/passwd
-	then echo "usuario $user_name encontrado"
-else
-	echo "usuario $user_name no encontrado"
-fi
